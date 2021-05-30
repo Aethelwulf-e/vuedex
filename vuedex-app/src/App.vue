13facfa1b95a9f276b8b8105e73fdc060122cb7d
carlos <carlosferreira@alu.ufc.br>
2021-05-29 21:04:47 -0300

    App init + vuetify + navbar

---
+++ vuedex-app/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-app-bar app color="black" dark>
+    <v-app-bar-nav-icon></v-app-bar-nav-icon>
+
+    <v-app-bar-title class="nav-bar-title">{{ Title }}</v-app-bar-title>
+
+    <v-spacer></v-spacer>
+
+    <v-btn icon>
+      <v-icon>mdi-magnify</v-icon>
+    </v-btn>
+  </v-app-bar>
+</template>
+
+<style>
+.nav-bar-title {
+  font-size: 1.70rem !important;
+  margin-left: 30px;
+}
+</style>
+
+<script>
+export default {
+  name: "App",
+
+  components: {},
+
+  data: () => ({
+    Title: "Vuedex",
+  }),
+};
+</script>
